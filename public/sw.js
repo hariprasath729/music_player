683edbf0cca3fbfa20e9f3ed8771e7d0c84c0147
hariprasath729 <hariprasathmohan729@gmail.com>
2026-07-08 22:57:45 +0530

    fixed logo problem

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Hardcode the version here. Update this string to trigger a new app update!
-const CACHE_NAME = "music-player-v12.4.21";
+const CACHE_NAME = "music-player-v12.4.22";
 
 // Files to cache (basic UI)
 const ASSETS_TO_CACHE = [
