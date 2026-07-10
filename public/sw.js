9b56ae24e4e03d8a0bdc4ea5146e7925d7e7b085
hariprasath729 <hariprasathmohan729@gmail.com>
2026-07-10 23:42:21 +0530

    music-player-v12.5.7.5

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Hardcode the version here. Update this string to trigger a new app update!
-const CACHE_NAME = "music-player-v12.5.7.4";
+const CACHE_NAME = "music-player-v12.5.7.5";
 
 // Files to cache (basic UI)
 const ASSETS_TO_CACHE = [
